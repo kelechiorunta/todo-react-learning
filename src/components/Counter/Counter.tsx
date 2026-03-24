7c5653272fc2b8d741c645f3f41edca1c47d8ece
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-03-24 13:54:35 +0100

    add Counter component

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,13 @@
+// Replace the local variable with useState hook using index as the state variable and setIndex as the setter function
+
+
+// export default function Counter() {
+//     let index: number = 0;
+
+//     function handleClick() {
+//     index = index + 1;
+//   }
+//     return <><h1>Counter</h1><p>{index}</p> <button onClick={handleClick}>Add One</button></>
+// }
+
+
