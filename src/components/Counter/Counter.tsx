ba907b02456a8044df31c4777223bc80b0b3074e
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-03-24 17:28:07 +0100

    add editor config file for code formatting

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 // Replace the local variable with useState hook using index as the state variable and setIndex as the setter function
 
-
 export default function Counter() {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    function handleClick() {
-     setIndex(index + 1);
+  function handleClick() {
+    setIndex(index + 1);
   }
-    return <><h1>Counter</h1><p>{index}</p> <button onClick={handleClick}>Add One</button></>
+  return (
+    <>
+      <h1>Counter</h1>
+      <p>{index}</p> <button onClick={handleClick}>Add One</button>
+    </>
+  );
 }
-
-
