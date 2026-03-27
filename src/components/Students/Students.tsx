eaadd7cbac90a603f8f528d17b91a912fb97d7f3
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-03-27 22:56:48 +0100

    add students task

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -1,15 +1,25 @@
 /**
  * Title: Students' List
  * Instructions
- * Create a student state variable(which is an array) using useState
- * Pass in the students array below as an initialState value
+ * Create an allStudents state variable(which is an array) using useState
+ * Pass in the students array below as an initialState value to the alStudents
+ * Create a student state variable object with name, course and level properties initialized to empty strings
+ * Map the allStudents array to form a list of updated students
  * Choose your state structure wisely
  *
  */
 
 import React from "react";
 
-const students = [{ name: "Tega", course: "React", level: "intermediate" }];
+interface StudentType {
+  name: string;
+  course: string;
+  level: string;
+}
+
+const students: StudentType[] = [
+  { name: "Tega", course: "React", level: "intermediate" },
+];
 
 export default function Students() {
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -57,6 +67,16 @@ export default function Students() {
           />
         </label>
       </div>
+      {/* Uncomment this and complete the mapping of the students */}
+      {/* <ul>
+        {allStudents.map((s: StudentType) => (
+          <>
+            <li>{s.name}</li>
+            <li>{s.course}</li>
+            <li>{s.level}</li>
+          </>
+        ))}
+      </ul> */}
       <button onClick={() => addStudent()}>Add Student</button>
     </div>
   );
